49ba4a9f79c2205f00f38e0ac3151c9335f0776d
pavan-patel-93 <pavanpatel2023@gmail.com>
2024-11-27 05:45:01 +0530

    Committed programs based on trees

---
+++ Trees/21_printRootToNodePath.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int value){
+        val = value;
+        left = right = nullptr;
+    };
+};
+
+class Solution{
+    public:
+    
+};
+
+
+int main(){
+}
